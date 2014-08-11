fc489de7fb5131761c8cc1adf1b0ad8034fc2702
Greg77630 <gregory.stone@gmail.com>
2014-08-11 07:50:54 -0500

    NewProjectServiceManager

---
+++ ServiceManager/ServiceManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceManager.Startup))]
+namespace ServiceManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
